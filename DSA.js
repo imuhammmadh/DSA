b6c3c22b1f9321370136eadf2209e99582c6a4c5
imuhammmadh <imuhammmadh@gmail.com>
2023-08-09 10:55:05 +0530

    secondLargest

--- DSA.js
+++ DSA.js
@@ -1,7 +1,37 @@
-let arr = [3, 2, 1, 5, 2];
-let largest = arr[0];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest)
-        largest = arr[i];
-};
-console.log(largest);
+
+
+
+
+
+
+
+
+let arr = [1, 2, 3, 4]
+function sLargest(a) {
+    let largest = a[0];
+    let sLarge = -Infinity;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > largest) {
+            sLarge = largest;
+            largest = a[i];
+        } else if (a[i] < largest && a[i] > sLarge) {
+            sLarge = a[i];
+        }
+    }
+    return sLarge;
+}
+console.log(sLargest(arr));
+function sSmallest(a) {
+    let smallest = a[0];
+    let sSmall = Infinity;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] < smallest) {
+            sSmall = smallest;
+            smallest = a[i];
+        } else if (a[i] !== smallest && a[i] < sSmall) {
+            sSmall = a[i];
+        }
+    }
+    return sSmall
+}
+console.log(sSmallest(arr));
