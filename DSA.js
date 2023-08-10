9fe6838e3874366f180394210a16837a2b840809
imuhammmadh <imuhammmadh@gmail.com>
2023-08-10 12:04:12 +0530

    .

--- DSA.js
+++ DSA.js
@@ -46,3 +46,13 @@ function isSorted(arr) {
     return true;
 }
 console.log(isSorted([1, 2, 3, 4, 5, 6]));
+
+function rotate(arr) {
+    let temp = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        arr[i - 1] = arr[i];
+    }
+    arr[arr.length - 1] = temp;
+    return arr;
+}
+console.log(rotate([1, 2, 3, 4, 5]));
